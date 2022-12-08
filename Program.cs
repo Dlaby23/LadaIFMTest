b6fa4c8865f231480fd76044c231fb94d0a6addf
1006 <1006@student.itgymnazium.cz>
2022-12-08 09:09:20 +0100

    final

---
+++ Program.cs
@@ -0,0 +1,52 @@
+﻿using System.IO.Pipes;
+
+namespace TipTheNumber
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            start:
+            int randomnum;
+            Random random = new Random();
+            randomnum = random.Next(1, 101);
+            Console.WriteLine("Zadejte číslo mezi 1-100");
+            int pokus = 0;
+            while (true)
+            {                
+                //Console.WriteLine(randomnum);
+                int tip = Convert.ToInt32(Console.ReadLine());
+                if (tip == randomnum)
+                {
+                    Console.WriteLine("výhra");
+                    Console.WriteLine(" počet pokusů: " + pokus);
+                    Console.WriteLine("chceš hrát znovu? Ano/Ne");
+                    var input = Console.ReadLine();
+                    if (input == "Ano")
+                    {
+                        goto start;
+                    }
+                    else
+                    {
+                        break;
+                    }
+                    
+                }
+                else if (tip < randomnum)
+                {
+                    Console.WriteLine("číslo je větší");
+                    pokus++;
+                }
+                else if (tip > randomnum)
+                {
+                    Console.WriteLine("číslo je menší");
+                    pokus++;
+                }
+                else
+                {
+                    Console.WriteLine("si dement, jen čísla!");
+                }
+            }
+        }
+    }
+}
